39a49dd182bec7eed55e773fded17bb14dd928be
Mario A. Mania <mario.mania@gmail.com>
2021-03-21 20:12:11 -0300

    fix tests

--- project/tests/test_summaries_unit.py
+++ project/tests/test_summaries_unit.py
@@ -24,63 +24,6 @@ def test_create_summaries_invalid_json(test_app):
     assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
 
 
-@pytest.mark.parametrize(
-    "summary_id, payload, status_code, detail",
-    [
-        [
-            999,
-            {"url": "https://foo.bar", "summary": "updated!"},
-            404,
-            "Summary not found",
-        ],
-        [
-            0,
-            {"url": "https://foo.bar", "summary": "updated!"},
-            422,
-            [
-                {
-                    "loc": ["path", "id"],
-                    "msg": "ensure this value is greater than 0",
-                    "type": "value_error.number.not_gt",
-                    "ctx": {"limit_value": 0},
-                }
-            ],
-        ],
-        [
-            1,
-            {},
-            422,
-            [
-                {
-                    "loc": ["body", "url"],
-                    "msg": "field required",
-                    "type": "value_error.missing",
-                },
-                {
-                    "loc": ["body", "summary"],
-                    "msg": "field required",
-                    "type": "value_error.missing",
-                },
-            ],
-        ],
-        [
-            1,
-            {"url": "https://foo.bar"},
-            422,
-            [
-                {
-                    "loc": ["body", "summary"],
-                    "msg": "field required",
-                    "type": "value_error.missing",
-                }
-            ],
-        ],
-    ],
-)
-def test_update_summary_invalid(test_app, monkeypatch, summary_id, payload, status_code, detail):
-    pass
-
-
 def test_update_summary_invalid_url(test_app):
     response = test_app.put(
         "/summaries/1/",
